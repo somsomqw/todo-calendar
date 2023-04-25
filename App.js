7cc4500a7d8de25890e4889305972379bfc7c10a
somsom <bin1209r@naver.com>
2023-04-25 18:43:14 +0900

    calendar ui create

--- App.js
+++ App.js
@@ -1,11 +1,130 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, Image } from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import dayjs from "dayjs";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import { Ionicons } from "@expo/vector-icons";
+
+import { getCalendarColumns } from "./src/util";
+import { useEffect, useState } from "react";
+import { runPracticeDayjs } from "./src/practice-dayjs";
+import Margin from "./src/Margin";
+import { useCalendar } from "./src/hook/use-calendar";
+import { useTodoList } from "./src/hook/use-todo-list";
+import Calendar from "./src/Calendar";
+
+const statusBarHeight = getStatusBarHeight(true);
 
 export default function App() {
+  const now = dayjs();
+
+  const {
+    selectedDate,
+    setSelectedDate,
+    isDatePickerVisible,
+    showDatePicker,
+    hideDatePicker,
+    handleConfirm,
+    subtract1Month,
+    add1Month,
+  } = useCalendar(now);
+
+  const { todoList } = useTodoList(selectedDate);
+
+  const columns = getCalendarColumns(selectedDate);
+
+  const onPressLeftArrow = subtract1Month;
+  const onPressHeaderDate = showDatePicker;
+  const onPressRightArrow = add1Month;
+  const onPressDate = setSelectedDate;
+
+  const ListHeaderComponent = () => (
+    <View>
+      <Calendar
+        todoList={todoList}
+        columns={columns}
+        selectedDate={selectedDate}
+        onPressLeftArrow={onPressLeftArrow}
+        onPressHeaderDate={onPressHeaderDate}
+        onPressRightArrow={onPressRightArrow}
+        onPressDate={onPressDate}
+      />
+      <Margin height={15} />
+
+      <View
+        style={{
+          width: 4,
+          height: 4,
+          borderRadius: 4 / 2,
+          backgroundColor: "#a3a3a3",
+          alignSelf: "center",
+        }}
+      />
+      <Margin height={15} />
+    </View>
+  );
+
+  const renderItem = ({ item: todo }) => {
+    const isSuccess = todo.isSuccess;
+    return (
+      <View
+        style={{
+          flexDirection: "row",
+          width: 220,
+          //backgroundColor: todo.id % 2 === 0 ? "pink" : "lightblue",
+          alignSelf: "center",
+          paddingVertical: 10,
+          paddingHorizontal: 5,
+          borderBottomWidth: 0.5,
+          borderColor: "#a6a6a6",
+        }}
+      >
+        <Text style={{ flex: 1, fontSize: 14, color: "#595959" }}>
+          {todo.content}
+        </Text>
+
+        <Ionicons
+          name="ios-checkmark"
+          size={17}
+          color={isSuccess ? "#595959" : "#bfbfbf"}
+        />
+      </View>
+    );
+  };
+
+  useEffect(() => {
+    runPracticeDayjs();
+  }, []);
+  useEffect(() => {
+    //console.log("selectedDate", dayjs(selectedDate).format("YYYY.MM.DD"));
+  }, [selectedDate]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={{
+          // 출처: https://kr.freepik.com/free-photo/white-crumpled-paper-texture-for-background_1189772.htm
+          uri: "https://img.freepik.com/free-photo/white-crumpled-paper-texture-for-background_1373-159.jpg?w=1060&t=st=1667524235~exp=1667524835~hmac=8a3d988d6c33a32017e280768e1aa4037b1ec8078c98fe21f0ea2ef361aebf2c",
+        }}
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+        }}
+      />
+
+      <FlatList
+        data={todoList}
+        ListHeaderComponent={ListHeaderComponent}
+        contentContainerStyle={{ paddingTop: statusBarHeight }}
+        renderItem={renderItem}
+      />
+
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+      />
     </View>
   );
 }
@@ -13,8 +132,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
